cb8795387aa03736e025e8095a98c907676eaadb
chmendonca <chmendonca1977@gmail.com>
2020-06-27 19:38:13 -0300

    Added a function to update the site update date
    The function to update at a single change the site update information was implemented on JS script and at all scripts that already exist.
    Also this hint was inserted on hintsHtml5Css3.html page

--- _js/functions.js
+++ _js/functions.js
@@ -2,3 +2,6 @@ var clicked = false
 function mudaFoto(foto){
     document.getElementById("picture").src=foto;
 }
+function atualizaData(){
+    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, June 27<sup>th</sup>";
+}
